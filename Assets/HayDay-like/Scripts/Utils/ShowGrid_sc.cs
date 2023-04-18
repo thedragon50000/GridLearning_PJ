3d8fd3fbdb7787f9e20693126d03c23b362ccdb4
Andy <s1810942008@gms.nutc.edu.tw>
2023-04-18 19:30:30 +0800

    ShowGrid.cs modified.

--- Assets/HayDay-like/Scripts/Utils/ShowGrid_sc.cs
+++ Assets/HayDay-like/Scripts/Utils/ShowGrid_sc.cs
@@ -7,28 +7,28 @@ using UnityEngine.Tilemaps;
 [RequireComponent(typeof(Grid))]
 public class ShowGrid_sc : MonoBehaviour
 {
-    // void OnDrawGizmos()
-    // {
-    //     Grid grid = GetComponent<Grid>();
-    //     Gizmos.color = Color.red;
-    //     
-    //     // for (int x = 0; x < grid.transform.lossyScale.x * grid.cellSize.x; x++)
-    //     for (int x = 0; x < 100; x++)
-    //     {
-    //         Gizmos.DrawLine(
-    //             grid.transform.position + new Vector3(grid.cellSize.x * x, 0, 0),
-    //             grid.transform.position +
-    //             new Vector3(grid.cellSize.x * x, 0, 100)
-    //         );
-    //     }
-    //
-    //     for (int y = 0; y < 100; y++)
-    //     {
-    //         Gizmos.DrawLine(
-    //             grid.transform.position + new Vector3(0, 0, grid.cellSize.y * y),
-    //             grid.transform.position +
-    //             new Vector3(100, 0, grid.cellSize.y * y)
-    //         );
-    //     }
-    // }
+    void OnDrawGizmos()
+    {
+        Grid grid = GetComponent<Grid>();
+        Gizmos.color = Color.red;
+        
+        // for (int x = 0; x < grid.transform.lossyScale.x * grid.cellSize.x; x++)
+        for (int x = 0; x < 100; x++)
+        {
+            Gizmos.DrawLine(
+                grid.transform.position + new Vector3(grid.cellSize.x * x, 0, 0),
+                grid.transform.position +
+                new Vector3(grid.cellSize.x * x, 0, -100)
+            );
+        }
+    
+        for (int y = 0; y < 100; y++)
+        {
+            Gizmos.DrawLine(
+                grid.transform.position + new Vector3(0, 0, -grid.cellSize.y * y),
+                grid.transform.position +
+                new Vector3(100, 0, -grid.cellSize.y * y)
+            );
+        }
+    }
 }
